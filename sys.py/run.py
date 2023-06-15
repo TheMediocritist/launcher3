6749f9d95b0c79538ba250aa7e76cb6204e8320e
TheMediocritist <79881777+TheMediocritist@users.noreply.github.com>
2023-06-15 14:07:08 +1000

    Update run.py

--- sys.py/run.py
+++ sys.py/run.py
@@ -349,7 +349,7 @@ def big_loop():
 if __name__ == '__main__':
     
     os.environ['SDL_VIDEO_CENTERED'] = '1'
-    X_center_mouse()
+    #X_center_mouse()
     
     os.chdir( os.path.dirname(os.path.realpath(__file__)) )
     
